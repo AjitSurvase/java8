ea018e318ef189f97814efe7d75cf6f86bd466d2
AjitSurvase <ajitsurvase71@gmail.com>
2025-12-29 01:46:41 +0530

    stream api

--- six.java
+++ six.java
@@ -3,11 +3,13 @@ package Stream_program;
 import java.util.Arrays;
 import java.util.List;
 
+
+// CONCEPT 4: Lazy Evaluation
 public class six {
 	
 	public static void main(String[] args) {
 		
-		
+//		Stream operations run only when needed.
 		List<Integer> nums = Arrays.asList(1, 2, 3);
 		
 		nums.stream()
@@ -16,6 +18,8 @@ public class six {
 			
 			return n>1;
 		});
+		
+//		(no output)
 		
 		System.out.println("***************");    
 		
@@ -28,7 +32,7 @@ public class six {
 		})
 		.forEach(System.out::println);
 		
-		
+//		Intermediate operations are lazy and executed only when a terminal operation is called.
 		
 		
 		
