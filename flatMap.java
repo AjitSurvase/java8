e4a3ebb511a04dfe17bdddd096c19765aebd5915
AjitSurvase <ajitsurvase71@gmail.com>
2025-12-30 17:03:23 +0530

    flatmap

---
+++ flatMap.java
@@ -0,0 +1,43 @@
+package Stream_program;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class flatMap {
+	
+	public static void main(String[] args) {
+//		❓ Problem Scenario
+
+//		You have nested data (list inside list).
+		
+		List<List<String>> names = Arrays.asList(
+			    Arrays.asList("Ajit", "Shivam"),
+			    Arrays.asList("Mahesh", "Ravi")
+			);
+		
+		
+		
+//		You want all names in one stream.
+		
+//		 What Happens with map() ❌
+		 names.stream()
+		.map(list -> list)
+		.forEach(System.out::println);
+		 
+		 
+		 
+		 
+		 
+//		 flatMap() transforms each element into a stream and then flattens all resulting streams into a single stream.
+		 
+//		 Stream of Stream → One Stream
+		  names.stream()
+		 .flatMap(list->list.stream())
+		 .forEach(System.out::println);
+		 
+		
+
+
+	}
+
+}
